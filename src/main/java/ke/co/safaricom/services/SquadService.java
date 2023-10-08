4bacc707e2c014b90d8babd85d1222f43530439a
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-10-09 00:31:48 +0300

    add public class SquadService

---
+++ src/main/java/ke/co/safaricom/services/SquadService.java
@@ -0,0 +1,24 @@
+package ke.co.safaricom.services;
+
+import ke.co.safaricom.models.Squad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SquadService {
+    private List<Squad> squadList = new ArrayList<>();
+
+    public void addSquad( List <Squad> mySquad, Squad squad){
+        if (mySquad != null){
+            squadList = mySquad;
+        } else {
+            mySquad = new ArrayList<>();
+        }
+        squad.setId(squadList.size()+1);
+        squadList.add(squad);
+    }
+    public List<Squad>getSquadList(){
+        return squadList;
+    }
+
+}
