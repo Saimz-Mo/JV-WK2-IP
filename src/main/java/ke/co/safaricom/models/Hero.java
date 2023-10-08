882e91044fc5bb7d37fd31f3a02f6053991ab420
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-10-09 00:15:08 +0300

    add public static list of get all heroes and set all heroes

--- src/main/java/ke/co/safaricom/models/Hero.java
+++ src/main/java/ke/co/safaricom/models/Hero.java
@@ -8,4 +8,23 @@ public class Hero {
     private String specialPower;
     private String weakness;
     private int heroId;
+
+    private static List<Hero>allHeroes = new ArrayList<>();
+
+    public static List<Hero> getAllHeroes() {
+        return allHeroes;
+    }
+
+    public static void  setAllHeroes(List<Hero> allHeroes) {
+        Hero.allHeroes = allHeroes;
+
+    }
+
+    public int getHeroId() {
+        return heroId;
+    }
+
+    public void setHeroId(int heroId) {
+        this.heroId = heroId;
+    }
 }
