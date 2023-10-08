de3a3ed229d2e2e1f1bba34de0f705c58b5def1a
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-10-08 23:50:14 +0300

    add public class App

--- src/main/java/ke/co/safaricom/App.java
+++ src/main/java/ke/co/safaricom/App.java
@@ -1,10 +1,8 @@
 package ke.co.safaricom;
 
-import java.util.HashMap;
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class App {
     public static void main(String[] args) {
         ProcessBuilder process = new ProcessBuilder();
         Integer port;
