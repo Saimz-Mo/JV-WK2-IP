e6c6a865fa749a81803c5e635f456f5d26e75909
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-10-09 00:13:09 +0300

    add class Hero

--- src/main/java/ke/co/safaricom/models/Hero.java
+++ src/main/java/ke/co/safaricom/models/Hero.java
@@ -1,4 +1,11 @@
 package ke.co.safaricom.models;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Hero {
+    private String name;
+    private String age;
+    private String specialPower;
+    private String weakness;
+    private int heroId;
 }
