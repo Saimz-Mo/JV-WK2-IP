3f5eed9ba21f416a0f79793e8a77588bebca76cd
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-10-08 23:32:33 +0300

    add src file

--- src/main/java/ke/co/safaricom/Main.java
+++ src/main/java/ke/co/safaricom/Main.java
@@ -6,6 +6,19 @@ import java.util.HashMap;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        }
+
+        else {
+            port = 4567;
+        }
+        port(port);
+
+        staticFileLocation("/public/");
 
 
 
