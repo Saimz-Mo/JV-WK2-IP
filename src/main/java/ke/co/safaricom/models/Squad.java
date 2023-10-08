217370f0d34ed8a8a2fb1ecb57379593f2b5815c
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-10-09 00:23:12 +0300

    add public static list for squad

--- src/main/java/ke/co/safaricom/models/Squad.java
+++ src/main/java/ke/co/safaricom/models/Squad.java
@@ -1,4 +1,16 @@
 package ke.co.safaricom.models;
 
 public class Squad {
+    private  String maxSize;
+    private  String squadName;
+    private String squadCause;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
