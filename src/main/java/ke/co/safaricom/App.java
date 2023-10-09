4bf13d18fbbdf173e5455ea451bc1225878e527f
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-10-09 09:40:34 +0300

    add file index css used for styling the application

--- src/main/java/ke/co/safaricom/App.java
+++ src/main/java/ke/co/safaricom/App.java
@@ -11,8 +11,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class App {
     public static void main(String[] args) {
         ProcessBuilder process = new ProcessBuilder();
